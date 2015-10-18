894bca23ca352a6cdb8c32c497911cf815d2923b
Callum Styan <callumstyan@gmail.com>
2015-10-17 19:43:52 -0700

    cfb not cf5

--- maybe_a_client.py
+++ maybe_a_client.py
@@ -29,8 +29,8 @@ args = parser.parse_args()
 
 command = "ls -l"
 sniffFilter = 'udp and dst port {0} and src port {1}' .format(args.sourcePort, args.destPort)
-encryptionObject = AES.new('This is a key123', AES.MODE_CF5, 'This is an IV456')
-decryptionObject = AES.new('This is a key123', AES.MODE_CF5, 'This is an IV456')
+encryptionObject = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
+decryptionObject = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
 encryptedCommand = encryptionObject.encrypt(command)
 packet = IP(dst=args.destIP)/UDP(dport=int(args.destPort), sport=int(args.sourcePort))/Raw(load=encryptedCommand)
 send(packet)
